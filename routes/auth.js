a84b23bb79d31af175125b2ee4902ea1f642eb6b
n07Laksh <112006645+n07Laksh@users.noreply.github.com>
2023-03-14 21:48:55 +0530

    bcrypt,jsonwebtoke,env file

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/Users");
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+//import the env files variables
+require("dotenv").config();
 
 
 // Create a User using POST "/api/auth/createuser" No login required
@@ -10,6 +15,8 @@ body("name","Please enter valid name").isLength({min:3}),
 body("email","Please enter unique email").isEmail(),
 body("password","password above 5 character").isLength({min:5})
 ,async(req, res) =>{
+
+  const secretKEy = process.env.SECRET_KEY;
   
     //return function return error if there is a error
     const errors = validationResult(req);
@@ -26,11 +33,30 @@ body("password","password above 5 character").isLength({min:5})
     if(user){
       return res.status(400).json({error: "Sorry a user with this email already exists"});
     }
+
+    // hashing password for security reasons
+    //creating a salt
+    const salt = await bcrypt.genSalt(10);
+    //creating a hash password
+    const password = await bcrypt.hash(req.body.password, salt);
+
+
+    //creating json web token
+    const data = {
+      user:{
+        id: User.id
+      }
+    }
+    const jwtAuth = jwt.sign(data,secretKEy);
+    console.log(jwtAuth);
+
+
+
     //Create a new User
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: password
       })
       res.json(user)
     } catch (error) {
@@ -40,5 +66,6 @@ body("password","password above 5 character").isLength({min:5})
     }
 
     });
+
 
 module.exports = router;
